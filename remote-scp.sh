e01cd6a9abc8145a4db08a0c30a222f269389b48
李俊杰 <phpor@users.noreply.github.com>
2016-08-03 11:15:33 +0800

    通过跳板机上传下载文件
    1. bash函数的多个参数，每个参数最好使用双引号，否则的话，如果前面某参数为空的话，就错位了
    2. 将命令参数原样传给某函数时，使用 $@ 也要使用双引号，否则参数 “a b”就变成两个参数了
    3. 按照某字段分隔读取到多个变量时，大可不必使用awk -F， 使用IFS指定分隔符就行了；但是有一点，read完之后最好立即重置IFS，否则影响后续执行；没有发现等价于IFS的read参数选项

---
+++ remote-scp.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+function make_tmp_filename() {
+	str='abcdefghigklmnopqrstuvwxyz'
+	echo .
+	for ((i=0; i<=4; i++)); {
+		echo ${str:$($RANDOM % 26)}
+	}
+}
+if [ "$SSH_KEY_FILE" == "" ];then
+	SSH_KEY_FILE=~/.ssh/id_rsa
+fi
+
+function upload() {
+	local src_file=$1 dst_ip=$2 dst_file=$3
+	local basename=$(basename $src_file)
+	if [[ "$dst_ip" == "" ]];then
+		scp -i $SSH_KEY_FILE $src_file root@$IP_BAOLEIJI:$dst_file && echo "done" || echo "upload fail"
+		exit $?
+	fi
+
+	scp -i $SSH_KEY_FILE $src_file root@$IP_BAOLEIJI:/tmp/$basename
+	if [ $? != 0 ]; then
+		exit 1
+	fi
+	ssh -tt -i $SSH_KEY_FILE root@$IP_BAOLEIJI "scp /tmp/$basename  root@$dst_ip:$dst_file;rm -f /tmp/$basename" 2>/dev/null
+	echo done
+}
+function download() {
+	local src_ip=$1 src_file=$2 dst_file=$3
+	local basename=$(basename $src_file)
+	if [[ "$src_ip" == "" ]]; then
+		scp -i $SSH_KEY_FILE root@$IP_BAOLEIJI:$src_file $dst_file  && echo "done" || echo "fail"
+		exit $?
+	fi
+	ssh -tt -i $SSH_KEY_FILE root@$IP_BAOLEIJI "scp root@$src_ip:$src_file /tmp/$basename "
+	if [ $? != 0 ]; then
+		exit 1
+	fi
+	scp -i $SSH_KEY_FILE root@$IP_BAOLEIJI:/tmp/$basename $dst_file
+	ssh -tt -i $SSH_KEY_FILE root@$IP_BAOLEIJI "rm /tmp/$basename " 2>/dev/null
+	echo done
+}
+function main(){
+	local method=$1
+	if [ -z $method ] || [[ "$method" != "upload" && "$method" != "download" ]];then
+		echo "Usage:"
+		echo "	$0 method ..."
+		exit 1
+	fi
+	shift
+	if [ $method == "upload" ];then
+		local src_file=$1 dst=$2
+		if [ -z $src_file ] || [ -z $dst ]; then
+			echo "Usage:"
+			echo "	$0 upload src_file dst_ip:dst_file"
+			exit 1
+		fi
+		echo $dst | (IFS=":"; read dst_ip dst_file;IFS="  "; upload "$src_file" "$dst_ip" "$dst_file")
+	fi
+	if [ $method == "download" ];then
+		local src=$1 dst_file=$2
+		if [ -z $dst_file ] || [ -z $src ]; then
+			echo "Usage:"
+			echo "	$0 download src_ip:src_file dst_file"
+			exit 1
+		fi
+		echo $src| (IFS=":"; read src_ip src_file;IFS="  ";	download "$src_ip" "$src_file" "$dst_file" )
+	fi
+}
+main "$@"
