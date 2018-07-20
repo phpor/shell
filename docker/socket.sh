3a7dd9a1971179adf1ec08908118533cc5c7c5f7
李俊杰 <phpor@users.noreply.github.com>
2018-07-20 16:20:12 +0800

    Update socket.sh
    添加退出操作

--- docker/socket.sh
+++ docker/socket.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+
 VOLUME_DIR=/data1/docker_volume
 MAINSOCKET=/var/run/docker-socket.sock
+__FILE__=$(cd $(dirname $0) && pwd)/$(basename $0)
 
 function main() {
+	for pid in $(getlastpids);do
+		[[ ! $pid -eq $$ ]] && [[ -e /proc/$pid/cmdline ]] && echo Please kill $pid first && exit
+	done
+
 	exec &>/dev/null
 	exec </dev/null
 	trap quit SIGINT SIGTERM
 
-	local self=$(cd $(dirname $0) && pwd)/$(basename $0)
+	local self=$__FILE__
 	local mainsocket=$MAINSOCKET
 
 	while :; do
@@ -20,7 +26,7 @@ function main() {
 			else
 				[[ -e $unixsock ]] && {
 					fuser -k $unixsock &>/dev/null
-					rm -fr $unixsock
+					rm -f $unixsock
 				}
 			fi
 		done
@@ -72,17 +78,29 @@ function server() {
 	done
 }
 
+function getlastpids() {
+	pgrep -af $__FILE__ |grep -v quit|awk '{print $1}'
+}
+
 function quit() {
-	find $VOLUME_DIR/*/config/.control.sock 2>/dev/null | while read unixsock;do
-		fuser -k $unixsock &>/dev/null
-		rm -fr $unixsock
-	done
-	fuser -k $MAINSOCKET
-	exit
+        find $VOLUME_DIR/*/config/.control.sock 2>/dev/null | while read unixsock;do
+                fuser -k $unixsock &>/dev/null
+                rm -f $unixsock
+        done
+        fuser -k $MAINSOCKET
+        rm -f $MAINSOCKET
+        exit
 }
 
 [[ $1 == 'handler' ]] && {
-	server $2
-	exit
+        server $2
+        exit
+}
+
+[[ $1 == 'quit' ]] && {
+        for pid in $(getlastpids); do
+                kill $pid
+        done
+        exit
 }
 main $*
